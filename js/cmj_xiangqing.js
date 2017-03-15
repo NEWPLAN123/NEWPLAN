6e53deb062b93b53f5f93d0577b368b20d999500
常敏杰 <2366132826@qq.com>
2017-03-15 09:57:20 +0800

    klk

---
+++ js/cmj_xiangqing.js
@@ -0,0 +1,15 @@
+var xxk=document.querySelector(".xxk");
+var gz=document.querySelectorAll(".xxk li");
+var ka=document.querySelector(".ka")
+var zp=document.querySelectorAll(".ka div");
+console.log(zp)
+for(i=0;i<gz.length;i++){
+    gz[i].index=[i];
+    gz[i].onclick=function(){
+        for(y=0;y<zp.length;y++){
+            zp[y].style.display="none";
+        }
+        this.className="active";
+        zp[this.index].style.display="block";
+    }
+}
