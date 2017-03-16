4076ce8a6fbbbfef71261561ed31f41555e2ba4c
Zhangjiaqiang1234 <810751959@qq.com>
2017-03-16 19:13:46 +0800

    'a'

--- php/login.php
+++ php/login.php
@@ -2,14 +2,11 @@
 
 <?php
 
-// session_start();
-
-
-
-echo 456123465;
+//开启session
+ session_start();
 
 //登录
-// $type = $_REQUEST['type']; //确定登录的方式
+ $type = $_REQUEST['type']; //确定登录的方式
 
 // if($type == 'phone'){   //如果是和手机的方式登录
 //     $acc = $_REQUEST['phone'];
@@ -17,30 +14,33 @@ echo 456123465;
 //     $acc = $_REQUEST['email'];
 // }
 
-// $password = md5($_REQUEST['password']);
+ $password = md5($_REQUEST['password']);
 
 //先查找用户名
-// $sql = "select * from login where $acc='$acc'";
+ $sql = "select * from login where $type='$type'";
 
-// $rul = $mysqli->query($sql);
-// if($rul->num_rows){
-//     //继续查询密码
-//     $sql = "select * from login where $acc='$acc' and password='$password'";
-//     $rul = $mysqli->query($sql);
-//     if($rul->num_rows){
-//         //登录成功
-//         $row = $rul->fetch_assoc();
-//         echo "登陆成功";
-//         //设置session变量
-//         $_SESSION['is_login'] = true;
-//         $_SESSION['lid'] = $row['lid'];
-//     }else{
-//         echo "密码错误";
-//         //跳转回登录页面并给出提示信息
-//     }
-// }else{  //如果没找到，说明用户名不存在
-//     echo '用户名不存在';
-// }
+ var_dump($sql);
+ exit();
+
+ $rul = $mysqli->query($sql);
+ if($rul->num_rows){
+     //继续查询密码
+     $sql = "select * from login where $acc='$acc' and password='$password'";
+     $rul = $mysqli->query($sql);
+     if($rul->num_rows){
+         //登录成功
+         $row = $rul->fetch_assoc();
+         echo "登陆成功";
+         //设置session变量
+         $_SESSION['is_login'] = true;
+         $_SESSION['lid'] = $row['lid'];
+     }else{
+         echo "密码错误";
+         //跳转回登录页面并给出提示信息
+     }
+ }else{  //如果没找到，说明用户名不存在
+     echo '用户名不存在';
+ }
 
 
 echo 4;
