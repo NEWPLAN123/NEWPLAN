34594d65602f90afe9f88211c3d0dab9075edead
Tubaobaosx <992618913@qq.com>
2017-03-20 20:03:32 +0800

    kx
    1111111111

--- js/directive.js
+++ js/directive.js
@@ -26,6 +26,20 @@ direct.directive("cnitem",function(){
             var kssearch=document.querySelector('#ks_search');
             var kbb=document.querySelector('.kx_search_quxiao');
             var kcc=document.querySelector('.kx_search_box');
+            // kaa.addEventListener('touchstart',function () {
+            //     kssearch.style.display='inline-block';
+            //     kaa.style.display='none';
+            //     kbb.style.display='inline-block';
+            //     kaab.style.display='inline-block';
+            //     kaac.style.display='none';
+            // });
+            // touch.on('.kx_search_right2','touchend',function (){
+            //     kssearch.style.display='inline-block';
+            //     kaa.style.display='none';
+            //     kbb.style.display='inline-block';
+            //     kaab.style.display='inline-block';
+            //     kaac.style.display='none';
+            // });
             kaa.onclick=function(){
                 kssearch.style.display='inline-block';
                 kaa.style.display='none';
@@ -33,6 +47,22 @@ direct.directive("cnitem",function(){
                 kaab.style.display='inline-block';
                 kaac.style.display='none';
             };
+            // touch.on('.kx_search_quxiao','touchstart',function () {
+            //     kssearch.style.display='none';
+            //     kbb.style.display='none';
+            //     kaa.style.display='inline-block';
+            //     kcc.style.display='none';
+            //     kaab.style.display='none';
+            //     kaac.style.display='inline-block';
+            // });
+            // kbb.addEventListener('touchstart',function () {
+            //     kssearch.style.display='none';
+            //     kbb.style.display='none';
+            //     kaa.style.display='inline-block';
+            //     kcc.style.display='none';
+            //     kaab.style.display='none';
+            //     kaac.style.display='inline-block';
+            // });
             kbb.onclick=function () {
                 kssearch.style.display='none';
                 kbb.style.display='none';
